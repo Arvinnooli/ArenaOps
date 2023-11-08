a5060687f0bf52e927531a0a553f8c8d2ead1645
Arvinnooli <arvinnooli711@gmail.com>
2023-11-08 10:26:11 +0530

    changed password in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='1234'
+app.config['MYSQL_PASSWORD']='lmao#711'
 app.config['MYSQL_DB']='stadium_database'
 
 db=MySQL(app)
